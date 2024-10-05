334268c9b1107529007b48a8f8b4865e906eb236
Baptiste Dromer <baptiste.dromer@gmail.com>
2024-10-05 10:38:31 +0200

    feat: 4.2 Blog List, step 2

---
+++ utils/middleware.js
@@ -0,0 +1,32 @@
+const logger = require('./logger')
+
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:  ', request.path)
+  logger.info('Body:  ', request.path)
+  logger.info('Path:  ', request.path)
+  logger.info('---')
+  next()
+}
+
+const notFound = (request, response) => {
+  response.status(404).json({ error: 'Not Found' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).json({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  requestLogger,
+  notFound,
+  errorHandler,
+}
