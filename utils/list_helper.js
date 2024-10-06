39bdf5f70180b4c1205b174b42b9e8a51c85942c
Baptiste Dromer <baptiste.dromer@gmail.com>
2024-10-06 21:05:16 +0200

    feat: 4.5*: Helper Functions and Unit Tests, step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,4 +3,9 @@ const dummy = (blogs) => 1
 const totalLikes = (blogs) =>
   blogs.length === 0 ? 0 : blogs.reduce((acc, blog) => acc + blog.likes, 0)
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) =>
+  blogs.length === 0
+    ? {}
+    : blogs.reduce((acc, val) => (acc.likes >= val.likes ? acc : val), blogs[0])
+
+module.exports = { dummy, totalLikes, favoriteBlog }
