b3b7140c9c0eb6eb15c522e029c0c7e2e26d1d7d
Baptiste Dromer <baptiste.dromer@gmail.com>
2024-10-06 22:55:55 +0200

    feat: 4.6*: Helper Functions and Unit Tests, step 4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const { countBy, maxBy } = require('lodash')
+
 const dummy = (blogs) => 1
 
 const totalLikes = (blogs) =>
@@ -8,4 +10,36 @@ const favoriteBlog = (blogs) =>
     ? {}
     : blogs.reduce((acc, val) => (acc.likes >= val.likes ? acc : val), blogs[0])
 
-module.exports = { dummy, totalLikes, favoriteBlog }
+const mostBlogs2 = (blogs) => {
+  const authorCount = blogs.reduce((acc, blog) => {
+    acc[blog.author] = (acc[blog.author] || 0) + 1
+    return acc
+  }, {})
+
+  let maxAuthor = ''
+  let maxBlogs = 0
+
+  for (const author in authorCount) {
+    if (authorCount[author] > maxBlogs) {
+      maxAuthor = author
+      maxBlogs = authorCount[author]
+    }
+  }
+  return { author: maxAuthor, blogs: maxBlogs }
+}
+
+const mostBlogs = (blogs) => {
+  const authorCount = countBy(blogs, 'author')
+
+  const authorWithMostBlogs = maxBy(
+    Object.keys(authorCount),
+    (author) => authorCount[author]
+  )
+
+  return {
+    author: authorWithMostBlogs || '',
+    blogs: authorCount[authorWithMostBlogs] || 0,
+  }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs }
