a97197558e9c85bd6a0dd2d41b86112aed980494
Baptiste Dromer <baptiste.dromer@gmail.com>
2024-10-20 16:25:01 +0200

    feat: 4.20*: Blog List Expansion, step 8

--- utils/middleware.js
+++ utils/middleware.js
@@ -8,6 +8,14 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  }
+  next()
+}
+
 const notFound = (request, response) => {
   response.status(404).json({ error: 'Not Found' })
 }
@@ -38,4 +46,5 @@ module.exports = {
   requestLogger,
   notFound,
   errorHandler,
+  tokenExtractor,
 }
